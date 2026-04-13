3f18cd26a71988410a02f36404a9d49c844f5da0
KK Tech <dyxj6n786p@privaterelay.appleid.com>
2026-04-13 11:12:43 +0100

    Completed some few imrovments

--- script.js
+++ script.js
@@ -7,6 +7,37 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  const episodeCount = document.createElement("p");
+  episodeCount.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.appendChild(episodeCount);
+
+  const credit = document.createElement("p");
+  credit.innerHTML =
+    'Data from <a href="https://tvmaze.com" target="_blank">TVMaze.com</a>';
+  rootElem.appendChild(credit);
+
+  for (const episode of episodeList) {
+    const card = makeEpisodeCard(episode);
+    rootElem.appendChild(card);
+  }
+}
+
+function makeEpisodeCard(episode) {
+  const seasonNum = String(episode.season).padStart(2, "0");
+  const episodeNum = String(episode.number).padStart(2, "0");
+  const episodeCode = "S" + seasonNum + "E" + episodeNum;
+
+  const card = document.createElement("div");
+  card.classList.add("episode-card");
+
+  card.innerHTML = `
+    <h2>${episode.name} - ${episodeCode}</h2>
+    <img src="${episode.image.medium}" alt="${episode.name}" />
+    ${episode.summary}
+  `;
+
+  return card;
 }
 
 window.onload = setup;
